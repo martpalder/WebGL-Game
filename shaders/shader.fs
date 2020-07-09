3b9c4b553785100d3c849ca3905a2037855fc0e6
martpalder <56435324+martpalder@users.noreply.github.com>
2020-07-09 19:25:21 +0300

    Update shader.fs

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 varying highp vec2 vTextureCoord;
 varying highp vec3 vLighting;
 
@@ -9,15 +8,3 @@ void main(void) {
 	
 	gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
 }
-=======
-varying highp vec2 vTextureCoord;
-varying highp vec3 vLighting;
-
-uniform sampler2D uSampler;
-
-void main(void) {
-	highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
-	
-	gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
-}
->>>>>>> eef9478f40cc009588c6b5695770292fd19006d0
