5b014ab079d932fcbef5e6376775a31fd334c453
martpalder <56435324+martpalder@users.noreply.github.com>
2020-07-13 20:41:13 +0300

    Update filereader.js

--- js/filereader.js
+++ js/filereader.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function readTextFile(url) {
 	const request = new XMLHttpRequest();
 	var contents
@@ -16,22 +15,3 @@ function readTextFile(url) {
 	
 	return contents;
 }
-=======
-function readTextFile(url) {
-	const request = new XMLHttpRequest();
-	var contents
-	
-	// When the file is loaded
-	request.onload = function() {
-		// Get the file contents
-		contents = request.response;
-	};
-	request.open("GET", url, false)
-	request.overrideMimeType("text/plain");
-	request.send();
-	if (request.status != 200)
-		console.log("Error " + request.status + ": '" + url + "' was not loaded!");
-	
-	return contents;
-}
->>>>>>> c9b3e4327f7f5d69c556a229066f97ce016b975d
